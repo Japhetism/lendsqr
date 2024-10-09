e4b2c6b7a52befcafbce81cb80ce3feb22cf1347
Babatunde Ojo <babatundeojo30@gmail.com>
2024-10-09 17:59:56 +0100

    add login images

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,26 @@
+import "./login.scss"
+
 const Login = () => {
     return (
-        <div>Login Page</div>
+        <div className="login-section">
+            <div className="login-banner">
+                <div className="logo">
+                    <img src="/images/logo.png" alt="logo" />
+                </div>
+                <div className="pablo">
+                    <img src="/images/pablo-sign.png" alt="sign-in" />
+                </div>
+            </div>
+            <div className="login-form">
+                <h2>Welcome!</h2>
+                <h4>Enter details to login.</h4>
+                <form>
+                    <div className="form-group">
+                    <input type="text" id="username" required />
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
