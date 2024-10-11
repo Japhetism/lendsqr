cb27ab9d51b52e589aabdae633454ea17c988fd3
Babatunde Ojo <babatundeojo30@gmail.com>
2024-10-11 09:04:21 +0100

    implement sidebar menu

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,20 @@
 import Header from "../../component/header";
+import SideMenu from "../../component/sidemenu";
+import "./dashboard.scss";
 
 const Dashboard = () => {
     return (
-        <Header />
-    )
+        <div className="App">
+            <Header />
+            <div className="main-content">
+                <SideMenu />
+                <div className="content">
+                    <h2>Welcome to My App</h2>
+                    <p>This is the main content area.</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Dashboard;
